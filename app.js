96e75edf2bd724d33d29acfb656765b1bcc6c4f2
codyjhill99 <codyjhill99@gmail.com>
2016-10-23 17:13:20 -0400

    added a basic server test

--- app.js
+++ app.js
@@ -1,60 +1,42 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//Lets require/import the HTTP module
+var http = require('http');
+var dispatcher = require('httpdispatcher');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//Lets define a port we want to listen to
+const PORT=8080;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+//We need a function which handles requests and send response
+//Lets use our dispatcher
+function handleRequest(request, response){
+  try {
+    //log the request on console
+    console.log(request.url);
+    //Disptach
+    dispatcher.dispatch(request, response);
+  } catch(err) {
+    console.log(err);
+  }
 }
+//Create a server
+var server = http.createServer(handleRequest);
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+//Lets start our server
+server.listen(PORT, function(){
+  //Callback triggered when server is successfully listening. Hurray!
+  console.log("Server listening on: http://localhost:%s", PORT);
 });
 
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
 
-module.exports = app;
+//A sample GET request
+dispatcher.onGet("/page1", function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Page One');
+});
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Got Post Data');
+});
